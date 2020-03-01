9eaf5d5ebc1af571c327b7b0815e5d5dd54eaa55
kenchangh <guanhao3797@gmail.com>
2020-03-01 22:20:28 +0000

    hashes

--- data_reader.py
+++ data_reader.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 from parser import get_aggregate_numbers_from_file
 
 DIRNAME = 'csse_covid_19_daily_reports'
@@ -11,15 +12,26 @@ def get_all_data_files_in_order():
     return filelist
 
 
+def hash_file_content(filepath):
+    m = hashlib.sha256()
+    with open(filepath) as f:
+        m.update(f.read().encode('utf-8'))
+    return m.hexdigest()
+
+
 def show_time_series_of_aggregates():
     filepaths = get_all_data_files_in_order()
     filepaths = [os.path.join(DIRNAME, f) for f in filepaths]
     timeseries = []
+    hashes = []
 
     for filepath in filepaths:
+        filehash = hash_file_content(filepath)
         aggregate = get_aggregate_numbers_from_file(filepath)
         timeseries.append(aggregate)
+        hashes.append(filehash)
     print(timeseries)
+    print(hashes)
 
 
 if __name__ == '__main__':
